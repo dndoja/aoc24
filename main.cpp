29765407d3c90cb2e26786fa5cdb42507605494a
dndoja <ndoja.damian@gmail.com>
2024-12-01 16:24:29 +0100

    setup: add build script

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main(){
-    std::printf("bruh\n");
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if (argc < 2) {
+        cout << "error: no day number passed!\n";
+        return 1;
+    }
+
+    int day_nr = atoi(argv[1]);
+    cout << day_nr << "\n";
+
     return 0;
 }
